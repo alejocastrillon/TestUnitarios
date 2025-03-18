7de3e201cfeadaeff5e272a1e3786054c9c1626e
alejocastrillon <alejandro.castrillon@utp.edu.co>
2025-03-18 17:52:00 -0500

    Parametrizacion del proceso

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int a = 5;
-        int b = 10;
+        int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
         System.out.println("La suma de %d y %d es: %d".formatted(a, b, Calculadora.suma(a, b)));
     }
 }
